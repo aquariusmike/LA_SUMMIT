77047843b78dea7d4daaa614616366fa0d3058d0
Mike Frost <141159387+aquariusmike@users.noreply.github.com>
2024-11-22 14:59:24 +0630

    Update MeetingTypeList.tsx

--- components/ui/MeetingTypeList.tsx
+++ components/ui/MeetingTypeList.tsx
@@ -70,10 +70,12 @@ const MeetingTypeList = () => {
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
-              img="/icons/add-meeting.svg"
-              title="New Meeting"
-              description="Start an instant meeting"
-              handleClick={() => setMeetingState('isInstantMeeting')} className={''}     />
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState('isInstantMeeting')}
+        className=""
+      />
       <HomeCard
         img="/icons/join-meeting.svg"
         title="Join Meeting"
@@ -132,17 +134,18 @@ const MeetingTypeList = () => {
         </MeetingModal>
       ) : (
         <MeetingModal
-                      isOpen={meetingState === 'isScheduleMeeting'}
-                      onClose={() => setMeetingState(undefined)}
-                      title="Meeting Created"
-                      handleClick={() => {
-                          navigator.clipboard.writeText(meetingLink);
-                          toast({ title: 'Link Copied' });
-                      } }
-                      image={'/icons/checked.svg'}
-                      buttonIcon="/icons/copy.svg"
-                      className="text-center"
-                      buttonText="Copy Meeting Link" children={undefined}        />
+          isOpen={meetingState === 'isScheduleMeeting'}
+          onClose={() => setMeetingState(undefined)}
+          title="Meeting Created"
+          handleClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            toast({ title: 'Link Copied' });
+          }}
+          image="/icons/checked.svg"
+          buttonIcon="/icons/copy.svg"
+          className="text-center"
+          buttonText="Copy Meeting Link"
+        />
       )}
 
       <MeetingModal
@@ -161,12 +164,13 @@ const MeetingTypeList = () => {
       </MeetingModal>
 
       <MeetingModal
-              isOpen={meetingState === 'isInstantMeeting'}
-              onClose={() => setMeetingState(undefined)}
-              title="Start an Instant Meeting"
-              className="text-center"
-              buttonText="Start Meeting"
-              handleClick={createMeeting} children={undefined}      />
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+      />
     </section>
   );
 };
